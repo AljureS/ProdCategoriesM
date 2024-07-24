5c9a67a4f25d537963de566af439c689cda1c96e
AljureS <saidsimon2@gmail.com>
2024-07-24 10:22:47 -0500

    Structure of the product service

---
+++ backend/product-service/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { ProductController } from './product.controller';
+
+@Module({
+  imports: [ProductModule],
+  controllers: [AppController, ProductController],
+  providers: [AppService],
+})
+export class AppModule {}
