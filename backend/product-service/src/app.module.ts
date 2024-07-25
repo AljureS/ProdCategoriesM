f4e3991385e79dc08e57841ea202c49b264ed608
AljureS <saidsimon2@gmail.com>
2024-07-25 09:33:35 -0500

    Conexion to postgre of product service

--- backend/product-service/src/app.module.ts
+++ backend/product-service/src/app.module.ts
@@ -2,11 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {config as dotenvConfig} from 'dotenv';
+dotenvConfig({path: '.development.env'});
 
 @Module({
-  imports: [ProductModule],
-  controllers: [AppController, ProductController],
+  imports: [
+    ProductModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',  
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password:  process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'], 
+      logging: true,
+      synchronize: true,
+      dropSchema: true,
+
+    })
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
